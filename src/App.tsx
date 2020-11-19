9f661d85162ab4778c2cbcc5d282fbc945d98cc0
eduardoRduraes <eduardoduras.bsi@gmail.com>
2020-11-19 00:03:19 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+      </p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,7 +17,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+      </a>
       </header>
     </div>
   );
